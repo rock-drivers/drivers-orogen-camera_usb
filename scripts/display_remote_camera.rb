da70ec547ab76e03a8f38d2028bd72bc428e284a
Rimres-gcs2 <rimres@dfki.de>
2012-11-26 17:39:18 +0100

    fix display_remote_script that uses the default orocos nameserver.

--- scripts/display_remote_camera.rb
+++ scripts/display_remote_camera.rb
@@ -4,10 +4,27 @@
 require 'orocos'
 require 'vizkit'
 include Orocos
-Nameservice::enable(:CORBA, :host => "192.168.111.60")
+Orocos::CORBA.call_timeout = 40000
+Orocos::CORBA.connect_timeout = 10000
+
+# Overrides default corba nameserver
+Orocos::CORBA.name_service.ip = "10.250.8.21" 
+
+# uses default- and addtional corba nameserver
+#Orocos::name_service << Orocos::CORBA::NameService.new("10.250.8.21")
 Orocos.initialize
 
-camera = TaskContext.get 'camera_usb_deployment'  
+camera = TaskContext.get 'payloaditem_3_cam_emi'  
+
+frame_reader = camera.frame.reader
+
+#while(true)
+#    sample = frame_reader.read
+#
+#    if sample
+#        puts "Frame: #{sample.image.size}"
+#    end
+#end
 
 Vizkit.display camera.frame
 Vizkit.exec
